06852c7867af9f5b75dc707da0b78124e43755da
zckennedy <78164161+zckennedy@users.noreply.github.com>
2022-03-20 14:07:01 -0400

    local json data base has been established and have began dev for app interaction with db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  // add task (verified)
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task} 
+    setTasks([...tasks, newTask])
+  }
+  // delete task (verified)
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id != id))
+  }
+
+  // getTasks (from server) 
+  const fetchTasks = async () => {
+    const res = await fetch(`http://localhost:3000/posts`)
+    const data = await res.json()
+    return data
+  }
+  const getTasks = async () => {
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+  }
+  useEffect(() => getTasks(),[])
+  
+
   return (
     <div className="App">
       <header className="App-header">
